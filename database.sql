236bd0bd9e3a1180d8ebb83f80164238dd6e0cd5
Moch. Shidqul Ahdi <shidqul.naser@gmail.com>
2022-08-10 02:54:29 +0700

    Edit struktur tabel kolom deadline

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@ CREATE TABLE task (
     id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
     detail TEXT NOT NULL,
     assignee VARCHAR(100) NOT NULL,
-    deadline TIMESTAMP NULL,
+    deadline DATE NULL,
     status ENUM('undone', 'done') DEFAULT 'undone',
     PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
